b5e367eadbeaf7fb56bcb92f2915cb3beee446a4
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-05-17 12:03:00 +0200

    Move group to config

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,9 @@ FROM node:lts-stretch-slim
 RUN apt-get update && apt-get install git -y
 RUN npm install -g messer
 
-ADD ./send_lucky_number.sh /usr/local/bin
+ADD . /usr/local/bin
 
 WORKDIR /home
-RUN chmod +x /usr/local/bin/send_lucky_number.sh
 
 
 
--- send_lucky_number.sh
+++ send_lucky_number.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+set -euo pipefail
+
 export NUM=$(messer --command='history "Elitarny numerek" 1' | grep "Dzisiejszy numerek to" | tail -c 4)
 
-export NAME=$(echo $NUM | ./num_to_name.js)
+export NAME=$(echo $NUM | num_to_name.js)
+export GROUP=$(node -e "let config = require(\"/root/.messer/tmp/config.json\"); console.log(config[\"group\"])")
 
-messer --command="message \"Krzysiu na przewodniczącego\" Dzisiaj szczęśliwy numerek ma $NAME"
+if [ $NAME != "" ]; then
+    messer --command="message \"$GROUP\" Dzisiaj szczęśliwy numerek ma $NAME"
+fi
