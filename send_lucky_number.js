901a4b3a131dcd06e850be7362210abf87d7b4cb
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-05-27 10:47:12 +0200

    Add logging

--- send_lucky_number.js
+++ send_lucky_number.js
@@ -5,35 +5,46 @@ const config = require("/root/.messer/tmp/config.json");
 
 const messer = new Messer();
 
+const log = console.log;
+
+console.log = function() {
+  log.apply(console, [Date(Date.now()).toString()].concat(arguments));
+};
+
 messer.messen
   .login()
   .then(() => messer.processCommand(`history "Elitarny numerek" 1`))
   .then(res => {
     const lucky = res.substr(31, 2);
+    console.log(`Lucky number: ${lucky}`);
     luckyOne = config[parseInt(lucky)];
+    console.log(`Lucky person: ${luckyOne}`);
     return luckyOne;
   })
   .then(luckyOne => {
     messer.messen.api.getUserID(luckyOne, (err, data) => {
+      console.log("Sending message...");
       let message = {
         body: `Dzisiaj szczęśliwy numerek ma @${luckyOne}`
       };
       if (err) {
+        console.log(`Could not get id of ${luckyOne}`);
         messer.messen.api.sendMessage(message, config.group);
-        return console.error(err);
+      } else {
+        const id = data[0].userID;
+        messer.messen.api.sendMessage(
+          {
+            ...message,
+            mentions: [
+              {
+                tag: `@${luckyOne}`,
+                id: id
+              }
+            ]
+          },
+          config.group
+        );
       }
-      const id = data[0].userID;
-      messer.messen.api.sendMessage(
-        {
-          ...message,
-          mentions: [
-            {
-              tag: `@${luckyOne}`,
-              id: id
-            }
-          ]
-        },
-        config.group
-      );
+      return console.log("Message sent");
     });
   });
