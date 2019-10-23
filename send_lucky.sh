fe4f17140fcbb4860b5680e119afb8484724d52c
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-10-23 11:19:23 +0200

    Fix stdout output

--- send_lucky.sh
+++ send_lucky.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-lucky=`python3 get_lucky/get_lucky.py | bash | python3 get_lucky/parse_response.py`
+lucky=`docker run -it --rm -e VULCAN_LOGIN=$VULCAN_LOGIN -e VULCAN_PASSWORD=$VULCAN_PASSWORD get_lucky:latest | tail -n 1`
 
 echo $lucky
 
-docker run --name lucky --restart=on-failure -v ~/.messer/tmp:/root/.messer/tmp -e LUCKY="$lucky" pniedzwiedzinski/lucky
+docker run --name send_lucky -it -v ~/.messer/tmp:/root/.messer/tmp -e LUCKY="$lucky" pniedzwiedzinski/lucky
 
-docker rm lucky
+docker rm send_lucky
